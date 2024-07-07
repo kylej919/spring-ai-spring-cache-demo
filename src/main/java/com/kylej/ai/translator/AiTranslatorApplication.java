02dc2d51edf4d4b693e3a3867afc7ba282cdb2fd
Kyle Johnson <kylej0217@gmail.com>
2024-07-07 15:38:21 -0600

    shell

---
+++ src/main/java/com/kylej/ai/translator/AiTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.kylej.ai.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kylej/ai/translator/AiTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylej.ai.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
